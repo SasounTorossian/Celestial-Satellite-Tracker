c27ad45729a55fd72d4e660889a2081670452f95
Sasoun Torossian <sasountorossian@gmail.com>
2019-04-27 10:42:57 +0100

    Overhaul of entire code. Began using pigpio for servo control.

--- Tracker.py
+++ Tracker.py
@@ -8,58 +8,151 @@ import math
 import geomag
 import os
 import RPi.GPIO as GPIO
+import pigpio
+import subprocess
 from skyfield.api import load
 from skyfield.api import Topos
+# Python QT for GUI in project!!!
 
-def setup_stepper():
-    global DIR
-    DIR = 20   #Direction of GPIO Pin
-    global STEP
-    STEP = 21  # Step GPIO Pin
-    global SLP
-    SLP = 16   # Sleep Pin
-    MODE = (6, 13, 19) # Microstep Resolution GPIO Pins
+def start_pigpiod():
+    out = subprocess.Popen(['sudo', 'pigpiod'], 
+						universal_newlines = True,
+						stdout=subprocess.PIPE, 
+						stderr=subprocess.STDOUT)
+    time.sleep(2)
+                        
+def start_gpiod():
+    out = subprocess.Popen(['sudo', 'gpsd', '/dev/ttyAMA0', '-F', '/var/run/gpsd.socket'], 
+						universal_newlines = True,
+						stdout=subprocess.PIPE, 
+						stderr=subprocess.STDOUT)
+    time.sleep(2)
+                        
+def setup_gpio():
+    gpio = pigpio.pi()
+    return gpio
+
+def setup_stepper(gpio):
+    print("setup_stepper")
     global DELAY
     DELAY = 0.01 #Delay between steps
+    
+    global DIR
+    DIR = 20   #Direction of GPIO Pin
+    
+    global STEP
+    STEP = 21  # Step GPIO Pin
+    
+    global SLP
+    SLP = 16   # Sleep Pin
+    
+    # Microstep Resolution GPIO Pins
+    MODE_PIN1 = 6
+    MODE_PIN2 = 13
+    MODE_PIN3 = 19
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(DIR, GPIO.OUT)
-    GPIO.setup(STEP, GPIO.OUT)
-    GPIO.setup(SLP, GPIO.OUT)
-    GPIO.setup(MODE, GPIO.OUT)
-    RESOLUTION = {'Full': (0, 0, 0),
-                  'Half': (1, 0, 0),
-                  '1/4': (0, 1, 0),
-                  '1/8': (1, 1, 0),
-                  '1/16': (0, 0, 1),
-                  '1/32': (1, 0, 1)}
-    GPIO.output(MODE, RESOLUTION['1/8'])
+    gpio.set_mode(DIR, pigpio.OUTPUT)
+    gpio.set_mode(STEP, pigpio.OUTPUT)
+    gpio.set_mode(SLP, pigpio.OUTPUT)
+    
+    gpio.set_mode(MODE_PIN1, pigpio.OUTPUT)
+    gpio.set_mode(MODE_PIN2, pigpio.OUTPUT)
+    gpio.set_mode(MODE_PIN3, pigpio.OUTPUT)
+    
+    #RESOLUTION
+    #'Full': (0, 0, 0),
+    #'Half': (1, 0, 0),
+    #'1/4': (0, 1, 0),
+    #'1/8': (1, 1, 0), SELECTED
+    #'1/16': (0, 0, 1),
+    #'1/32': (1, 0, 1)}
+    
+    gpio.write(MODE_PIN1, 1)
+    gpio.write(MODE_PIN2, 1)
+    gpio.write(MODE_PIN3, 0)
+    
 
-def setup_servo():
-    servoPIN = 18
-    GPIO.setup(servoPIN, GPIO.OUT)
-    pwm = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
-    pwm.start(7.5)
-    return pwm
-
-def run_stepper(step_count, CW):
-    GPIO.output(DIR, CW)
-    #print('Exiting Sleep Mode')
-    GPIO.output(SLP, GPIO.HIGH)
+def run_stepper(angle, gpio):
+    print("run_stepper")
+    if(angle < 0):
+        angle += 360
+    print("current angle: {}".format(run_stepper.current_angle))
+    print("new angle: {}".format(angle))
+    print("change in angle: {}".format(angle - run_stepper.current_angle))
+    
+    if((angle - run_stepper.current_angle) < -180):
+        angle_change = 360 + (angle - run_stepper.current_angle)
+        
+    elif((angle - run_stepper.current_angle) > 180):
+        angle_change = (angle - run_stepper.current_angle) - 360
+        
+    else:
+        angle_change = angle - run_stepper.current_angle
+    
+    CW = 1 if angle_change >= 0 else 0
+    step_count = int(abs(angle_change)/1.8)
+    if(step_count <= 2):
+        print("step count: {}".format(step_count))
+        print("CW: {}".format(CW))
+        print("increment too small, no movement >:(")
+    else:
+        print("step count: {}".format(step_count))
+        print("CW: {}".format(CW))
+        run_stepper.current_angle = angle
+        print("updated current angle: {}".format(run_stepper.current_angle))
+    
+    gpio.write(DIR, CW)
+    
+    print('Exiting Sleep Mode')
+    gpio.write(SLP, 1)
+    
     for x in range (step_count*8):
-        GPIO.output(STEP, GPIO.HIGH)
+        gpio.write(STEP, 1)
         time.sleep(DELAY)
-        GPIO.output(STEP, GPIO.LOW)
+        gpio.write(STEP, 0)
         time.sleep(DELAY)
-    #print('Entering Sleep Mode')
-    GPIO.output(SLP, GPIO.LOW)
+    print('Entering Sleep Mode')
+    gpio.write(SLP, 0)
+run_stepper.current_angle = 0
 
-def run_servo(angle, pwm):
-    dc = (23/360)*angle + 7.5 #equation to convert angles to duty cycles
-    pwm.ChangeDutyCycle(dc)
-    time.sleep(0.1)
+def align_stepper(angle, gpio):
+    print("align_stepper")
+    if (angle <= 180):
+        CW = 0
+        step_count = int(angle/1.8)
+    else:
+        CW = 1
+        step_count = int((360-angle)/1.8)
+    
+    print("step count: {}".format(step_count*8))
+    print("CW: {}".format(CW))
+    gpio.write(DIR, CW)
+    
+    print('Exiting Sleep Mode')
+    gpio.write(SLP, 1)
+    
+    for x in range (step_count*8):
+        gpio.write(STEP, 1)
+        time.sleep(DELAY)
+        gpio.write(STEP, 0)
+        time.sleep(DELAY)
+    print('Entering Sleep Mode')
+    gpio.write(SLP, 0)
+
+def setup_servo(gpio):
+    print("setup_servo")
+    gpio.set_PWM_frequency(18,50)
+
+def run_servo(angle, gpio):
+    print("run_servo")
+    dc = (100/9)*angle + 1500
+    dc = round(dc, 0)
+    print(dc)
+    gpio.set_servo_pulsewidth(18, dc)
+    time.sleep(0.25)
 
 def setup_mag():
+    print("setup_mag")
     #initialise i2c comm on the RPi3
     i2c = busio.I2C(board.SCL, board.SDA)
     #initialise the lsm303 accelorometer via i2c comm
@@ -67,6 +160,7 @@ def setup_mag():
     return MAGsensor
 
 def setup_gps():
+    print("setup_gps")
     #listen on port 2947 (gpsd) of local
     GPSsensor = gps.gps('localhost', '2947')
     #start streaming gps data
@@ -74,11 +168,13 @@ def setup_gps():
     return GPSsensor
 
 def get_heading(sensor):
-    #retreive accelorometer data
+    print("get_heading")
+    '''
+    #retreive accelerometer data
     raw_acc_x, raw_acc_y, raw_acc_z = sensor.raw_acceleration
     raw_mag_x, raw_mag_y, raw_mag_z = sensor.raw_magnetic
 
-    #normalise the raw data from the X and Y axis of the accelorometer
+    #normalise the raw data from the X and Y axis of the accelerometer
     accXnorm = raw_acc_x/math.sqrt(raw_acc_x*raw_acc_x + raw_acc_y*raw_acc_y + raw_acc_z*raw_acc_z)
     accYnorm = raw_acc_y/math.sqrt(raw_acc_x*raw_acc_x + raw_acc_y*raw_acc_y + raw_acc_z*raw_acc_z)
 
@@ -86,173 +182,156 @@ def get_heading(sensor):
     pitch = math.asin(accXnorm)
     roll = -math.asin(accYnorm/math.cos(pitch))
 
-    #caculate new pitch compensated X and Y magenetic values based of accelorometer data
+    #caculate new pitch compensated X and Y magenetic values based of accelerometer data
     magXcomp = raw_mag_x*math.cos(pitch) + raw_mag_z*math.sin(pitch)
     magYcomp = raw_mag_x*math.sin(roll)*math.sin(pitch) + raw_mag_y*math.cos(roll) - raw_mag_z*math.sin(roll)*math.cos(pitch)
 
     #print heading and compensated heading
     #heading = (math.atan2(raw_mag_y,raw_mag_x)*180)/3.1415
-    heading_comp = (math.atan2(magYcomp, magXcomp)*180)/3.1415
+    heading = (math.atan2(magYcomp, magXcomp)*180)/3.1415
+    '''
+    acc_x, acc_y, acc_z = sensor.acceleration
+    mag_x, mag_y, mag_z = sensor.magnetic
+    heading = (math.atan2(mag_y,mag_x)*180)/3.14
+    
+    if(heading < 0):
+        heading += 360
+    
+    return heading
 
-    #if (heading_comp < 0):
-    #    heading_comp += 360
-
-    return heading_comp
-
-def align_stepper_north(trueNorth):
-    if(trueNorth > 0):
-        CW = 0
-    else:
-        CW = 1
-    alignStep = int(abs(trueNorth)/1.8)
-    run_stepper(alignStep, CW)
-
-def get_gps(session, heading):
-    tryConnect = 1
-    while(tryConnect == 1):
+def get_lat_lon(session):
+    print("get_lat_lon")
+    while(1):
         report = session.next()
-        #print(report)
-
+        #print(report) #prints raw incoming gps data
         if report['class'] == 'TPV':
-            tryConnect = 0
-            if hasattr(report, 'lat'):
+            if (hasattr(report, 'lat') and hasattr(report, 'lon')):
                 print('Latitude: ', report.lat)
-            if hasattr(report, 'lon'):
-                print('Longitude: ', report.lon)
-            if hasattr(report, 'alt'):
-                print('Altitude: ', report.alt)
-            getDec = 1
-            if (hasattr(report, 'lat') and hasattr(report, 'lon') and getDec == 1):
-                mag_dec = geomag.declination(report.lat, report.lon)
-                #print('Magnetic decination at local area is: ', mag_dec)
-                #print(' ')
-                get_dec = 0
-
-            trueNorth = heading +  geomag.declination(report.lat, report.lon)
-            print('True north is: {0:10.3f}'.format(trueNorth))
-            return trueNorth, report.lat, report.lon
-            #print('Magnetic north is: {0:10.3f}'.format(heading))
-            #print('Pitch compensated magnetic north is: {0:10.3f}'.format(heading_comp))
-            #print('Raw accelorometer data is: ({0:10.3f}, {0:10.3f}, {0:10.3f})'.format(raw_acc_x, raw_acc_y, raw_acc_z))
-            print(' ')
-
+                print('Longitude: ', report.lon)                
+                return report.lat, report.lon
+            else:
+                print('gps attributes not ready')
         else:
             print('Attempting to establish GPS connection')
             time.sleep(1)
+            
+def get_true_north(lat, lon, heading):
+    print("get_true_north")
+    mag_dec = geomag.declination(lat, lon)
+    print('heading at this location is: {}'.format(heading))
+    print('magnetic declination at this location is: {}'.format(mag_dec))
+    
+    if ((heading + mag_dec) < 0):
+        true_north = 360 + (heading + mag_dec)
+        
+    elif ((heading + mag_dec) > 360):
+        true_north = 360 - (heading + mag_dec)
+        
+    elif(heading > 180):
+        true_north = (heading - mag_dec)
+    
+    else:
+        true_north = (heading + mag_dec)
+        
+    print('True north is: {0:10.3f} from current position'.format(true_north))
+    return true_north
 
-def load_planet():
-    print('loading file')
+def load_planet(planet_select):
+    print("load_planet")
     planets = load('de421.bsp')
-    '''
-    switcher = {
-        'sun': planets['sun'],
-        'mercury': planets['mercury'],
-        'venus': planets['venus'],
-        'earth': planets['earth'],
-        'moon': planets['moon'],
-        'mars': planets['mars'],
-        'jupiter': planets['jupiter'],
-        'saturn': planets['saturn'],
-        'uranus': planets['uranus'],
-        'neptune': planets['neptune'],
-        'pluto': planets['pluto'],
-    }
-    '''
-    earth = planets['earth']
-    mars = planets['mars']
-    moon = planets['moon']
-    print('returning')
-    return earth, mars, moon
-    #return switcher.get(planet, 'pass')
+    return planets[planet_select]
 
-def load_satellites():
+def load_satellites(satellite_select):
+    print("load_satellites")
     stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
     satellites = load.tle(stations_url)
-    satellites = satellites['ISS (ZARYA)']
-    return satellites
+    satellite = satellites[satellite_select]
+    
+    #get time difference between now and satellite epoch
+    ts = load.timescale()
+    t = ts.now()
+    days = t - satellite.epoch
+    
+    #if difference is greater than 2 weeks, update tle with latest table
+    if (abs(days) > 14):
+        satellites = load.tle(stations_url, reload=True)
+        satellite = satellites[satellite_select]
+        
+    return satellite
 
 def load_current_location(earth, lat, lon):
+    print("load_current_location")
+    print(lat)
+    print(lon)
     location_astrometric = earth + Topos(lat, lon)
     location_topocentric = Topos(lat, lon)
     return location_astrometric, location_topocentric
 
 def get_planet_location(location_astrometric, planet):
+    print("get_planet_location")
     ts = load.timescale()
     t = ts.now()
     astrometric = location_astrometric.at(t).observe(planet)
     alt, az, dis = astrometric.apparent().altaz()
-    #altDec, azDec = degrees_to_decimal(alt, az)
-    print('Altitude: {}'.format(alt))
-    print('Azimuth: {}'.format(az))
-    #print('Altitude Decimal: {}'.format(altDec))
-    #print('Azimuth Decimal: {}'.format(azDec))
-    print('Distance: {}'.format(dis))
+    return alt.degrees, az.degrees, dis.km
 
 def get_satellite_location(location_topocentric, satellite):
+    print("get_satellite_location")
     ts = load.timescale()
     t = ts.now()
     difference = satellite - location_topocentric
     topocentric = difference.at(t)
     alt, az, dis = topocentric.altaz()
-    print('ISS Altitude: {}'.format(alt))
-    print('ISS Azimuth: {}'.format(az))
-    print('ISS Distance: {}'.format(dis))
-
-'''
-def degrees_to_decimal(latDeg, lonDeg):
-    latParts = re.split('[^\d\w]+', latDeg)
-    latDegD = latParts[0]
-    latDegM = latParts[1]
-    latDegS = latParts[2]
-    latDegDir = latParts[3]
-
-    latParts = re.split('[^\d\w]+', lonDeg)
-    lonDegD = lonParts[0]
-    lonDegM = lonParts[1]
-    lonDegS = lonParts[2]
-    lonDegDir = lonParts[3]
-
-    latDec = float(latDegD) + float(latDegM)/60 + float(latDegS)/(60*60);
-    if latDegDir == 'E' or latDegDir == 'N':
-        latDec *= -1
-
-    lonDec = float(lonDegD) + float(lonDegM)/60 + float(lonDegS)/(60*60);
-    if lonDegDir == 'E' or lonDegDir == 'N':
-        lonDec *= -1
-
-    return latDec, lonDec
-'''
-
-
+    return alt.degrees, az.degrees, dis.km
+    
 def main():
     #clear console screen
     os.system('clear')
-    #variable to retreive magnetic declination in local area on first run
+    start_pigpiod()
+    start_gpiod()
+    
+    #setup compass and gps
     MAGsensor = setup_mag()
     GPSsensor = setup_gps()
-    setup_stepper()
-    pwm = setup_servo()
+    
+    #set up stepper and servo motors
+    gpio = setup_gpio()
+    setup_stepper(gpio)
+    setup_servo(gpio)
 
+    #get heading, latitude, and longitude
     heading = get_heading(MAGsensor)
-    trueNorth, lat, lon = get_gps(GPSsensor, heading)
-    align_stepper_north(trueNorth)
-    print('Lat: ', lat)
-    print('Lon: ', lon)
-    print(' ')
+    lat, lon = get_lat_lon(GPSsensor)
+    
+    #obtain true north, then allign stepper with true north
+    true_north = get_true_north(lat, lon, heading)
+    align_stepper(true_north, gpio)
 
     print('loading planets')
-    earth, mars, moon = load_planet()
-    #mars =  load_planet()
+    earth =  load_planet('earth')
+    moon = load_planet('moon')
+    mars = load_planet('mars')
+    
     print('loading satellites')
-    iss = load_satellites()
+    iss = load_satellites('ISS (ZARYA)')
+    
     print('loading current location')
     loc_ast, loc_top = load_current_location(earth, lat, lon)
-    get_planet_location(loc_ast, mars)
-    print(' ')
-    get_planet_location(loc_ast, moon)
-    print(' ')
-    get_satellite_location(loc_top, iss)
 
+    
+    while(1):
+        alt, az, dis  = get_satellite_location(loc_top, iss)
+        #alt, az, dis  = get_planet_location(loc_ast, mars)
+        #alt, az, dis = get_planet_location(loc_ast, moon)
+        print('Altitude: {}'.format(alt))
+        print('Azimuth: {}'.format(az))
+        print('Distance: {}'.format(dis))
+        print(' ')
+        run_servo(alt, gpio)
+        run_stepper(az, gpio)
+        print('-----------------------------------------')
+        time.sleep(20)
+    
     time.sleep(1)
 
 if __name__ == '__main__':
@@ -261,9 +340,9 @@ if __name__ == '__main__':
     except KeyboardError:
         pass
     except KeyboardInterrupt:
-        GPIO.cleanup()
+        gpio.stop()
         quit()
     except StopIteration:
         session = None
     finally:
-        GPIO.cleanup()
+        gpio.stop()
